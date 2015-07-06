7114551dfb606d31ececf0e876ba07f1d5250783
Ambrou <apetitgenet@free.fr>
2015-07-06 19:24:19 +0100

    Initialisation

---
+++ QtUi/qtui.cpp
@@ -0,0 +1,12 @@
+#include "qtui.h"
+
+QtUi::QtUi(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+}
+
+QtUi::~QtUi()
+{
+
+}
---
+++ QtUi/qtui.h
@@ -0,0 +1,19 @@
+#ifndef QTUI_H
+#define QTUI_H
+
+#include <QtWidgets/QMainWindow>
+#include "ui_qtui.h"
+
+class QtUi : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	QtUi(QWidget *parent = 0);
+	~QtUi();
+
+private:
+	Ui::QtUiClass ui;
+};
+
+#endif // QTUI_H
